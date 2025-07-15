c8279f3a2a55c1c83f24084d44d0d4bf358fb740
SUJOY GARAI <sujoycode999@gmail.com>
2025-07-15 22:38:29 +0530

    update db names

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,6 +3,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URL
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["AuthPilot"]
-client_collections = db["client"]
-client_user_collections = db["users"]
+db = client["PlugAPI"]
+client_collections = db["CLIENTS"]
+client_user_collections = db["USERS"]
